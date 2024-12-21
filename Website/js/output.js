6b8e6e12dc7b901c0daf470f344e3951642e0f73
Masonlet <Masonletoile@hotmail.com>
2024-12-20 22:45:04 -0500

    Began implementing code to reduce motion on the page after each text is dynamically printed on page once

--- Website/js/output.js
+++ Website/js/output.js
@@ -2,127 +2,120 @@
 let activeRight = false;
 let leftString = "";
 let rightString = "";
-let disable = false;
+let typingSpeed = 6;
 
 window.onload = function() { 
     if (document.getElementById("homePage")) {
-        printLeft("about");
+        if (sessionStorage.getItem("aboutLoaded") === "true") {
+            document.getElementById("output").innerHTML = getAbout("about");
+            document.getElementById("output").innerHTML += `<footer><a id='button' class='hoverColor' style='margin-top:4vh;display:flex; font-size: 2vw;border: 2px solid #004d4d;border-radius: 6%;background: rgba(0, 0, 0, 0.10); width: 100%; justify-content: center; 'href='about.html'>Click for more information</a></footer>`;
+        } else {
+            printLeft("about");
+            sessionStorage.setItem("aboutLoaded", true);
+        }
     }
     else if (document.getElementById("aboutPage")) {
         printLeft("life");
         printRight("job1");
+        sessionStorage.setItem("life", true);
+        sessionStorage.setItem("job1", true);
     }
  };
 
+function getJob(selectedJob) {
+    switch (selectedJob) {
+        case "job1":
+            return "Insert some text about first job description, job role, skills required for job, etc etc..."
+        case "job2":
+            return "Insert some text about second job description, job role, skills required for job, etc etc...";
+        case "job3":
+            return "Insert some text about third job description, job role, skills required for job, etc etc...";
+    }
+}
+function updateJob(selectedJob) {
+    document.getElementById("job1").style.backgroundColor = selectedJob ? "rgba(0, 0, 0, 0.15)" : "transparent";
+    document.getElementById("job2").style.backgroundColor = selectedJob ? "rgba(0, 0, 0, 0.15)" : "transparent";
+    document.getElementById("job3").style.backgroundColor = selectedJob ? "rgba(0, 0, 0, 0.15)" : "transparent";
+}
+
+function getAbout(selectedSection) {
+    switch (selectedSection) {
+        case "life":
+            return "I was born in London, ontario and have lived in the surrounding area my entire life. I have traveled to every province in Canada and have been around a good chunk of America.<br>" +
+                "From a young age, I have always been extremely interested in technology, computers, and gaming. I've always had basic knowledge of how to work on computers by building them from a young age.<br>" +
+                "I dabled in programming in highschool, but I was more interested in automotive and mechanical work so I decided to start my college career at Fanshawe in their automotive technician program. After doing this for a year I realized mechanical work was more of a hobby than a career. I decided to flip the boat from the physcial work of a mechanic and pursue Computer Programming.<br>" +
+                "I enjoy programming more then I ever imagined I would. Now that I truly had the chance to dive into coding I have been so intrigued by the complexity and the problem solving that comes with it. I am now in my second year of the program and I am loving every second of it.<br>";
+        case "about":
+            return "Hello and welcome to my portfolio. I am currently a student at Fanshawe College, pursuing a degree in Computer Programming and Analysis.<br>" +
+                "I have a strong passion for learning, problem solving and have been completely deeply immersed in the fascinating and complex world that surrounds coding and programming.<br>" +
+                "While I have not yet commited to a specific specialization, my current objective is to broaden my knowledge and skills across as many areas as possible.<br>" +
+                "Thank you very taking the time to explore my portfolio, I hope you enjoy learning about my projects and getting to know me.<br>";
+        case "skills":
+            return "Front-end languages\n- JavaScript\n- CSS\n- HTML\n<Back-end languages\n- Java\n- C++\n- SQL";
+    }
+}
+function updateAbout(selectedSection) {
+    $("about").style.backgroundColor = selectedSection ? "rgba(0, 0, 0, 0.15)" : "transparent";
+    $("skills").style.backgroundColor = selectedSection ? "rgba(0, 0, 0, 0.15)" : "transparent";
+    $("contacts").style.backgroundColor = selectedSection ? "rgba(0, 0, 0, 0.15)" : "transparent";
+}
+
 function printRight(outputType) {
     if(!activeRight){
-        switch(outputType){ 
-            case "job1":
-                rightString = "Insert some text about first job description, job role, skills required for job, etc etc...";
-                document.getElementById("job1").style.backgroundColor = "rgba(0, 0, 0, 0.15)";
-                document.getElementById("job2").style.backgroundColor = "transparent";
-                document.getElementById("job3").style.backgroundColor = "transparent";
-                break;
-            case "job2":
-                rightString = "Insert some text about second job description, job role, skills required for job, etc etc...";
-                document.getElementById("job1").style.backgroundColor = "transparent";
-                document.getElementById("job2").style.backgroundColor = "rgba(0, 0, 0, 0.15)";
-                document.getElementById("job3").style.backgroundColor = "transparent";
-                break;
-            case "job3":
-                rightString = "Insert some text about third job description, job role, skills required for job, etc etc...";
-                document.getElementById("job1").style.backgroundColor = "transparent";
-                document.getElementById("job2").style.backgroundColor = "transparent";
-                document.getElementById("job3").style.backgroundColor = "rgba(0, 0, 0, 0.15)";
-                break;
+        rightString = getJob(outputType);
+        updateJob(outputType);
+
+        if (sessionStorage.getItem(`${outputType}Seen`) === "true") {
+            $("output2").innerHTML = rightString;
+            activeRight = false;
         }
-        if(document.getElementById("Output2").innerHTML !== ""){
-            document.getElementById("Output2").innerHTML = "";
+        else {
+            document.getElementById("output2").innerHTML = "";
+            activeRight = true;
+            output(true, 0);
+            sessionStorage.setItem(`${outputType}Seen`, "true");
         }
-       
-        activeRight = true;
-        output(true, 0);
     }
 }
 function printLeft(outputType) {
     if(!activeLeft){
-        switch(outputType){
-            case "life":
-                leftString = "I was born in London, ontario and have lived in the area my entire life. I have traveled to every province in canada and been around a good chunk of America.<" +
-                "Ever since a young age I have been extremely interested in technology, computers, and gaming. I've always had basic knowledge of how to work on computers by building them from a young age.\n" +
-                "I dabled in programming in highschool, but I was more interested in automotive and mechanical work so I decided to start my college career at Fanshawe in their automotive technician program. After doing this for a year I realized mechanical work was more of a hobby then a career. I decided to flip the boat and jump straight back into Fanshawe for Computer Programming.\n" +
-                "I enjoy programming more then I ever imagined I would. Now that I truly had the chance to dive into coding I have been so intrigued by the complexity and the problem solving that comes with it. I am now in my second year of the program and I am loving every second of it.<";
-                break;
-            case "about":
-                leftString = "Hello and welcome to my portfolio! I am currently enrolled at Fanshawe College pursuing a degree in Computer Programming and Analysis.<" + 
-                    "I have a strong passion for learning, problem solving and have been completely engulfed in the deep and complex world that surrounds coding and programming.<" +
-                    "I have not found a specialization that I have commited myself to yet. My current objective is to simply expand my knowledge and skills in as many areas as possible.<" +
-                    "Thank you very checking out my portfolio, I hope you enjoy exploring my projects and learning about me.<>";
-                document.getElementById("about").style.backgroundColor = "rgba(0, 0, 0, 0.15)";
-                document.getElementById("skills").style.backgroundColor = "transparent";
-                document.getElementById("contacts").style.backgroundColor = "transparent";
-                break;
-            case "skills":
-                leftString = "Front-end languages\n" +
-                "- JavaScript\n- CSS\n- HTML\n" +
-                "<Back-end languages\n" + 
-                "- Java\n- C++\n- SQL";
-                document.getElementById("about").style.backgroundColor = "transparent";
-                document.getElementById("skills").style.backgroundColor = "rgba(0, 0, 0, 0.15)";
-                document.getElementById("contacts").style.backgroundColor = "transparent";
-                break;
-        }
+        leftString = getAbout(outputType);
 
-        if(document.getElementById("Output").innerHTML !== ""){
-            document.getElementById("Output").innerHTML = "";
+        if (sessionStorage.getItem(`${outputType}Seen`) === "true") {
+            document.getElementById("output").innerHTML = leftString;
         }
-
-        activeLeft = true;
-        output(false, 0);
+        else {
+            document.getElementById("output").innerHTML = "";
+            activeLeft = true;
+            output(false, 0);
+            sessionStorage.setItem(`${outputType}Seen`, "true");
+        }
     }
 }
 
 //Function to add the next character to the output text
-function output(print2, index){
+function output(printRight, index){
+    let outputDiv = printRight ? document.getElementById("output2") : document.getElementById("output");
+    let currentString = printRight ? rightString : leftString;
 
-    let outputDiv = null;
-
-    if(print2) {
-        outputDiv = document.getElementById("Output2");
-    }
-    else {
-        outputDiv = document.getElementById("Output");
-    }
-    
     //End recursive function if we are at the end of the string
-    if(index === leftString.length || (print2 && index === rightString.length)){
-        if(print2){
-            activeRight = false;
-        }
-        else {
-            activeLeft = false;
-        }
+    if (index === currentString.length){
+        if (printRight) activeRight = false;
+        else activeLeft = false;
         return true;
     }
 
-
     //Next character to add
-
-    let nextChar;
-
-    if (print2) {
-        nextChar = rightString.charAt(index);
-    }
-    else {
-        nextChar = leftString.charAt(index);
-    }
+    let nextChar = currentString.charAt(index);
 
     //Add the next character to the output
     if(nextChar === '<'){
         outputDiv.innerHTML += "<div id = 'break'></div>";
     }
-    else if (nextChar === '>'){
-        outputDiv.innerHTML += "<a id='about' class='hoverColor' style='margin-top:4vh;display:flex; font-size: 2vw;border: 2px solid #004d4d;border-radius: 6%;background: rgba(0, 0, 0, 0.10); width: 100%; justify-content: center; 'href='about.html'>Click for more information about me</a>";
+    else if (nextChar === '>') {
+        const footer = `<footer><a id='button' class='hoverColor' style='margin-top:4vh;display:flex; font-size: 2vw;border: 2px solid #004d4d;border-radius: 6%;background: rgba(0, 0, 0, 0.10); width: 100%; justify-content: center; 'href='about.html'>Click for more information</a></footer>`;
+        outputDiv.innerHTML += footer;
+        setTimeout(() => { outputDiv.querySelector('footer').style.opacity = "1"; }, 10);
     }
     else if (nextChar === '\n') {
         outputDiv.innerHTML += "<br>";
@@ -131,9 +124,7 @@ function output(print2, index){
         outputDiv.innerHTML += nextChar;
     }
    
-
     //Recursively call this function with the next index
-    const delay = 6;
-    setTimeout(() => output(print2, (index + 1)), delay);
+    setTimeout(() => output(printRight, (index + 1)), 6);
     return true;
 }
