2ed5dbea011f37f948b007802689296f0bfaa7c1
Masonlet <Masonletoile@hotmail.com>
2025-01-17 17:42:11 -0500

    Finished/Fixed text typing and indexing

--- js/output.js
+++ js/output.js
@@ -1,62 +1,61 @@
+//Variables
 const TYPING_SPEED = 20;
 const HOME_IMAGE_PATHS = {
-    html: `img/Languages/html.png`,
-    css: `img/Languages/css.png`,
-    js: `img/Languages/js.png`,
-    java: `img/Languages/java.png`,
-    cpp: `img/Languages/cpp.png`,
-    cs: `img/Languages/cs.png`
-};
-const SECTIONS = [
-    `about`, 
-    `skills`, 
-    `contacts`
-];
+    html: `img/Languages/html.png`, css: `img/Languages/css.png`, js: `img/Languages/js.png`,
+    java: `img/Languages/java.png`, cpp: `img/Languages/cpp.png`, cs: `img/Languages/cs.png`
+};  
+const SECTIONS = [`about`, `skills`, `contacts`];
+const output = { leftOutput: null, rightOutput: null }
+const content = {
+    left: {
+        about: `Welcome to my portfolio.<br><br>
+                I am deeply invested in learning, problem-solving and exploring the fascinating world of programming.
+                While I have not yet committed to a specific specialization, my current focus is broadening my knowledge and skills across as many areas as possible.
+                I hope you enjoy learning about my projects and getting to know me.<br><br>
+                Throughout my time at fanshawe I have completed multiple projects that have spanned across various programs and languages,
+                giving me a good hands-on experience in different areas of computer science.
+                These projects have allowed me to translate theoretical knowledge into practical solutions for real-world problems,
+                ranging from creating CRUD applications to developing mathematical expression evaluators.<br><br>
+                Throughout this process, I have gained valuable experience with tools such as Visual Studio, Visual Studio Code, SQL Server Management Studio (SSMS), 
+                and have strengthened my understanding of fundamental concepts in debugging and problem solving.`
+    },
+    right: {}
+}
+
+//State Functions
+const createState = () => ({
+    typingLeft: false,
+    typingRight: false,
+    leftString: ``,
+    rightString: ``,
+    typingIndices: {
+        about: 0
+    },
+    currentSection: ``
+});
 
 function getState() {
-    let state = sessionStorage.getItem(`state`);
-    if (state){
-        return JSON.parse(state);
-    }
-    else {
-        state = {
-            typingLeft: false,
-            typingRight: false,
-            leftString: ``,
-            rightString: ``,
-            typingIndices: {
-                about: 0
-            }
-        }
-        sessionStorage.setItem(`state`, JSON.stringify(state));
-        return state;
-    }
-}
-const state = getState();
-
-const leftContent = {
-    about: `Welcome to my portfolio.<br><br>
-            I am deeply invested in learning, problem-solving and exploring the fascinating world of programming.
-            While I have not yet committed to a specific specialization, my current focus is broadening my knowledge and skills across as many areas as possible.
-            I hope you enjoy learning about my projects and getting to know me.<br><br>
-            Throughout my time at fanshawe I have completed multiple projects that have spanned across various programs and languages,
-            giving me a good hands-on experience in different areas of computer science.
-            These projects have allowed me to translate theoretical knowledge into practical solutions for real-world problems,
-            ranging from creating CRUD applications to developing mathematical expression evaluators.<br><br>
-            Throughout this process, I have gained valuable experience with tools such as Visual Studio, Visual Studio Code, SQL Server Management Studio (SSMS), 
-            and have strengthened my understanding of fundamental concepts in debugging and problem solving.`,
-    skills: createSkills()
-}
-const rightContent = {
-
+    const savedState = sessionStorage.getItem(`state`);
+    return savedState ? JSON.parse(savedState) : createState();
 }
 
+function updateState(key, value) {
+    const currentState = getState();
+    const newState = {...currentState, [key]: value};
+    sessionStorage.setItem(`state`, JSON.stringify(newState));
+    return newState;
+}
+
+//Helper Functions
 function createSkills() {
-    const createImage = (type) => `<img src="${HOME_IMAGE_PATHS[type]}" alt="${type} logo" class="language" loading="lazy">`;
+    if(content.left.skills) return content.left.skills;
+
+    const createImage = type => `<img src="${HOME_IMAGE_PATHS[type]}" alt="Logo of ${type}" class="language" loading="lazy">`;
     const frontEndImages = [`html`, `css`, `js`].map(createImage).join(``);
     const backEndImages = [`java`, `cpp`, `cs`].map(createImage).join(``);
 
-    return `<div id="skillsDiv">Front-end languages<div class = "imageOut">${frontEndImages}</div>Back-end languages<div class ="imageOut">${backEndImages}</div></div>`
+    content.left.skills = `<div id="skillsDiv">Front-end languages<div class = "imageOut">${frontEndImages}</div>Back-end languages<div class ="imageOut">${backEndImages}</div></div>`;
+    return content.left.skills;
 }
 
 function preloadImages(images) {
@@ -69,89 +68,101 @@ function preloadImages(images) {
                 img.src = path;
             });
         })
-    );
+    ).catch(error => {
+        console.error(`Failed to load images: ${error}`);
+    })
 }
 
-function highlightSection(selectedSection) {
-    SECTIONS.forEach(section => {
-        const element = document.getElementById(section);
-        if(element) element.style.backgroundColor = section === selectedSection ? "rgba(0, 0, 0, 0.5)" : "rgba(0, 0, 0, 0.25)"
-    });
-}
-
+//Typewriter Functions
 async function printContent(outputType, isRight = false) {
     const side = isRight ? `right` : `left`;
-    const typingKey = `typing${side.charAt(0).toUpperCase() + side.slice(1)}`;
-    const contentKey = `${side}String`;
-    const outputDiv = document.getElementById(`${side}Output`);
-    if (!outputDiv) return;
 
-    state[typingKey] = false;
-    highlightSection(outputType);
+    const outputDiv = document.getElementById(`${side}Output`);
+    if (!outputDiv) {
+        console.error(`Output div not found for ${side}`);
+        return;
+    }
+
+    //Check if the section is already being typed
+    const state = getState();
+    if (state.currentSection === outputType) return;
+
+    //Disable typing
+    const typingKey = `typing${side.charAt(0).toUpperCase() + side.slice(1)}`;
+    updateState(typingKey, false);
+
+    //Highlight section
+    updateState(`currentSection`, outputType);
+    SECTIONS.forEach(section => {
+        const element = document.getElementById(section);
+        if(element) element.style.backgroundColor = section === outputType ? "rgba(0, 0, 0, 0.5)" : "rgba(0, 0, 0, 0.25)"
+    }); 
 
     if (outputType === `skills`) {
         outputDiv.innerHTML = createSkills();
         return;
     }
 
-    const index = sessionStorage.getItem(`${outputType}Index`);
-    const content = isRight ? rightContent[outputType] : leftContent[outputType];
-    state[contentKey] = content;
-
-    if (index) {
-        state.typingIndices[outputType] = parseInt(index);
-        outputDiv.innerHTML = content.substring(0, state.typingIndices[outputType]);
-    }
-    else {
-        state.typingIndices[outputType] = 0;
-        outputDiv.innerHTML = ``;
+    const sideContent = content[side][outputType];
+    if (!sideContent) {
+        console.error(`Content not found for ${outputType}`);
+        return;
     }
 
-    state[typingKey] = true;
-    await typeContent(outputType, isRight);
-}
-let debounceTimer;
-async function typeContent(outputType, isRight){
-    const side = isRight ? `right` : `left`;
-    const typingKey = `typing${side.charAt(0).toUpperCase() + side.slice(1)}`;
     const contentKey = `${side}String`;
-    const outputDiv = document.getElementById(isRight ? `rightOutput` : `leftOutput`);
-
-    if(!state[typingKey] || !outputDiv) return;
-
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(async () => {
-        while(state[typingKey] && state.typingIndices[outputType] < state[contentKey].length) {
-            const index = state.typingIndices[outputType];
-            const nextChar = state[contentKey].charAt(index);
+    updateState(contentKey, sideContent);
+    updateState(typingKey, true);
     
-            if (nextChar === `<`) {
-                const tagEnd = state[contentKey].indexOf(`>`, index);
-                if(tagEnd === -1) return;
-                const tag = state[contentKey].substring(index, tagEnd + 1);
-                outputDiv.innerHTML += tag;
-                state.typingIndices[outputType] = tagEnd + 1;
-            }
-            else {
-                outputDiv.innerHTML += nextChar;
-                state.typingIndices[outputType]++;
-            }
-        
-            sessionStorage.setItem(`${outputType}Index`, state.typingIndices[outputType]);
-            await new Promise(resolve => setTimeout(resolve, TYPING_SPEED));
-        }
-       
-        state[typingKey] = false;
-    }, 100)
+    outputDiv.innerHTML = '';
+    await typeContent(outputType, outputDiv, typingKey, contentKey);
 }
 
-window.onload = async function () {
-    sessionStorage.clear();
-    const homePage = document.getElementById(`homePage`);
+async function typeContent(outputType, outputDiv, typingKey, contentKey){
+    let state = getState();
+    if(!state[typingKey]) { return; }
 
-    if (homePage) {
-        await preloadImages(HOME_IMAGE_PATHS);
-        highlightSection("about");
-        await printContent("about"); 
+    let index = state.typingIndices[outputType] || 0;
+    const content = state[contentKey];
+
+    if (index > 0){
+        outputDiv.innerHTML = content.substring(0, index);
     }
-};
+
+    while(index < content.length && state[typingKey]) {
+        if (content[index] === `<`) {
+            const tagEnd = content.indexOf(`>`, index);
+            if(tagEnd === -1) break;
+
+            outputDiv.innerHTML += content.substring(index, tagEnd + 1);
+            index = tagEnd + 1;
+        }
+        else {
+            outputDiv.innerHTML += content[index];
+            index++;
+        }
+    
+        state = updateState(`typingIndices`, {...state.typingIndices, [outputType]: index});
+        if (!state) return;
+
+        await new Promise(resolve => setTimeout(resolve, TYPING_SPEED));
+        state = getState();
+    }
+    
+    updateState(typingKey, false);
+}
+
+//Listeners
+if(document.getElementById(`homePage`)) {
+    document.getElementById(`about`).addEventListener(`click`, () => printContent(`about`));
+    document.getElementById(`skills`).addEventListener(`click`, () => printContent(`skills`));
+}
+window.addEventListener(`load`, async () => { 
+    const homePage = document.getElementById(`homePage`);
+    if(!homePage) return;
+
+    await preloadImages(HOME_IMAGE_PATHS);
+    let state = createState();
+    updateState(`currentSection`, ``);
+    updateState(`state`, state);
+    await printContent("about"); 
+});
