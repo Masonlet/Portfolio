c1f88f47ee99eba7dbaec2e2d8ec858bef8bd4c0
Masonlet <masonletoile@hotmail.com>
2025-11-20 15:31:31 -0500

    Rename languages folder to tech and update all related references

--- js/project.js
+++ js/project.js
@@ -4,132 +4,132 @@ const projectData = {
     description: 'Starlet Setup is a lightweight Python utility to quickly clone, configure, and build CMake projects — from single repos to full mono-repos.',
     image: 'img/projects/starlet-setup.png',
     github: 'https://github.com/masonlet/starlet-setup',
-    languages: ['python']
+    tech: ['python']
   },
   'tasktracker': {
     title: 'Task Tracker',
     description: 'TaskTracker is a lightweight tool for Windows 10 and 11 that adds task status options to the right-click context menu of folders.',
     image: 'img/projects/tasktracker.png',
     github: 'https://github.com/masonlet/tasktracker',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'opengl': {
     title: 'C++ OpenGL Engine',
     description: 'A C++ OpenGL engine ecosystem.',
     image: 'img/projects/opengl.png',
     github: 'https://github.com/masonlet/starletdevelopment',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'portfolio': {
     title: 'Portfolio',
     description: 'A personal site showcasing my skills and projects. Built with HTML, CSS, and JavaScript.',
     image: 'img/projects/portfolio.png',
     github: 'https://github.com/masonlet/portfolio',
-    languages: ['html', 'css', 'js']
+    tech: ['html', 'css', 'js']
   },
   'githubvisualizer': {
     title: 'GitHub Visualizer',
     description: 'A Python utility to explore and visualize GitHub repositories and their activity.',
     image: 'img/projects/githubvisualizer.png',
     github: 'https://github.com/masonlet/githubvisualizer',
-    languages: ['python']
+    tech: ['python']
   },
   'imagesandbox': {
     title: 'Image Sandbox',
     description: 'A C++ playground for experimenting with images loaded using StarletSerializer.',
     image: 'img/projects/imagesandbox.png',
     github: 'https://github.com/masonlet/starletimagesandbox',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'samples': {
     title: 'Starlet Samples',
     description: 'A repository for demonstrating the Starlet Engine, including sample scenes, meshes, textures, and shaders.',
     image: 'img/projects/samples.png',
     github: 'https://github.com/masonlet/starletsamples',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'noisesandbox': {
     title: 'Noise Sandbox',
     description: 'A C++ playground for learning and experimenting with noise algorithms.',
     image: 'img/projects/noise.png',
     github: 'https://github.com/masonlet/starletnoisesandbox',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'starter': {
     title: 'Starlet Starter',
     description: 'A Template for Starlet Game Projects.',
     image: 'img/projects/starter.png',
     github: 'https://github.com/masonlet/starletstarter',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'cardportfolio': {
     title: 'Card Portfolio',
     description: 'A personal site showcasing my skills and projects. Built with HTML, CSS, and JavaScript.',
     image: 'img/projects/card.png',
     github: 'https://github.com/masonlet/cardportfolio',
-    languages: ['html', 'css', 'js']
+    tech: ['html', 'css', 'js']
   },
   'graphics': {
     title: 'Starlet Graphics',
     description: 'Graphics loading & management library for Starlet projects.',
     image: 'img/projects/graphics.png',
     github: 'https://github.com/masonlet/starletgraphics',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'serializer': {
     title: 'Starlet Serializer',
     description: 'Serialization library for Starlet projects to handle both data reading and writing.',
     image: 'img/projects/serializer.png',
     github: 'https://github.com/masonlet/starletserializer',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'engine': {
     title: 'Starlet Engine',
     description: 'Modular OpenGL engine written in C++.',
     image: 'img/projects/engine.png',
     github: 'https://github.com/masonlet/starletengine',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'scene': {
     title: 'Starlet Scene',
     description: 'ECS-based scene & scene management library for Starlet projects.',
     image: 'img/projects/scene.png',
     github: 'https://github.com/masonlet/starletscene',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'logger': {
     title: 'Starlet Logger',
     description: 'Logging library for Starlet projects.',
     image: 'img/projects/logger.png',
     github: 'https://github.com/masonlet/starletlogger',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'controls': {
     title: 'Starlet Controls',
     description: 'Input management library for Starlet projects.',
     image: 'img/projects/controls.png',
     github: 'https://github.com/masonlet/starletcontrols',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'math': {
     title: 'Starlet Math',
     description: 'A lightweight header-only math library for Starlet projects.',
     image: 'img/projects/math.png',
     github: 'https://github.com/masonlet/starletmath',
-    languages: ['cpp']
+    tech: ['cpp']
   },
   'tests': {
     title: 'Starlet Tests',
     description: 'A repository containing unit tests for Starlet libaries using Goolge Test(gtest).',
     image: 'img/projects/tests.png',
     github: 'https://github.com/masonlet/starlettests',
-    languages: ['cpp']
+    tech: ['cpp']
   },
 };
 
 const IMAGE_PATHS = {
-  html: `img/languages/html.png`, css: `img/languages/css.png`, js: `img/languages/js.png`, 
-  python: `img/languages/python.png`, java: `img/languages/java.png`, cpp: `img/languages/cpp.png`, cs: `img/languages/cs.png`
+  html: `img/tech/html.png`, css: `img/tech/css.png`, js: `img/tech/js.png`, 
+  python: `img/tech/python.png`, java: `img/tech/java.png`, cpp: `img/tech/cpp.png`, cs: `img/tech/cs.png`
 };  
 
 const projectCards = document.querySelectorAll('.project-card');
@@ -137,14 +137,14 @@ const grid = document.querySelector('#projects-grid');
 const details = document.querySelector('#project-details');
 const ANIMATION_DURATION = 300;
 
-function createLanguageIcons(languages){
-  if (!languages || languages.length === 0) return '';
+function createTechIcons(icons){
+  if (!icons || icons.length === 0) return '';
 
-  const icons = languages
+  const iconData = icons
     .map(lang => `<img src="${IMAGE_PATHS[lang]}" alt="${lang}" class="language-icon" loading="lazy">`)
     .join('');
 
-  return `<div class="project-languages">${icons}</div>`;
+  return `<div class="project-tech">${iconData}</div>`;
 }
 
 function fadeTransition(hideElement, showElement, callback){
@@ -176,7 +176,7 @@ function showProjectDetails(projectKey) {
   fadeTransition(grid, details, () => {
     details.innerHTML = `
       <h3>${data.title}</h3>
-      ${createLanguageIcons(data.languages)}
+      ${createTechIcons(data.tech)}
       <div id="project-buttons">
         <button id="back-to-grid">← Back to Projects</button>
         <a href="${data.github}" target="_blank" id="github-link">
