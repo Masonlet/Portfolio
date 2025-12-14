04eb7a39b0efb5904a7871abdc0e3e515fbaf2e7
Mason L'Etoile <contact@masonletoile.ca>
2025-12-13 21:30:24 -0500

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -15,7 +15,7 @@ module.exports = async (req, res) => {
 
   try {
     await resend.emails.send({
-      from: 'onboarding@resend.dev',
+      from: 'noreply@masonletoile.ca',
       to: 'contact@masonletoile.ca',
       subject: `Contact form: ${subject}`,
       text: `From: ${email}\n\n${message}`
